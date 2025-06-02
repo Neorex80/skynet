242928bb10742350683ed5e88399d9c5d29e9ccc
Rushikesh Pawar <125253603+Neorex80@users.noreply.github.com>
2025-06-03 02:38:56 +0530

    Add useState import to ChatArea component

--- src/components/ChatArea.tsx
+++ src/components/ChatArea.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Message } from '../types';
 import { MessageBubble } from './MessageBubble';
 import { ArrowDownIcon } from '@heroicons/react/24/outline';
